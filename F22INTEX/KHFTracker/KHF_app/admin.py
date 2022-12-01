90ee78fb65d51ea4cd3179d26be37cc33218d50a
AndyGraviet <93163571+AndyGraviet@users.noreply.github.com>
2022-11-30 18:10:04 -0700

    Delete F22INTEX/KHFTracker directory

--- F22INTEX/KHFTracker/KHF_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import recommendation, user, meal, food_item, food_item_in_meal
-# Register your models here.
-admin.site.register(recommendation)
-admin.site.register(user)
-admin.site.register(meal)
-admin.site.register(food_item)
-admin.site.register(food_item_in_meal)
